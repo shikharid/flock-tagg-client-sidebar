18fc932d566b0e94e9929c745d9a971115e62420
vibhor <vibhor.go@directi.com>
2016-09-24 20:18:58 +0530

    added truncate

--- components/main.js
+++ components/main.js
@@ -22,7 +22,7 @@ angular.
 			controller: ['$scope', '$http', function($scope,$http){
 
 			  $scope.data = '{"files":[{"id":"123","file_data":"http://www.example.com/dir/file.html"},{"id":"456","file_data":"http://www.anotherexample.com/directory/hello.img"}]}';
-				$scope.message = '{"messages":[{"id":"123","message_content":"let the game begin"},{"id":"456","message_content":"it was elementary watson"}]}';
+				$scope.message = '{"messages":[{"id":"123","message_content":"let the game begin brhebv vreuhv vrever bnvuriuev nvernvrr rvnunuie newfneufnun nfiueifuj nfenfe f3woihw"},{"id":"456","message_content":"it was elementary watson"}]}';
 
 
 
@@ -54,7 +54,7 @@ angular.
 					for(var i in data.messages){
 						var obj=data.messages[i];
 
-						$scope.messages.push(obj.message_content.substr(0,15));
+						$scope.messages.push(obj.message_content);
 						//console.log(obj.file_data);
 					}
 					//console.log($scope.skills);
