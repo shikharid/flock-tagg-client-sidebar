e56cff580ec7265ce36bc862149577888cda4b4d
vibhor <vibhor.go@directi.com>
2016-09-25 05:12:49 +0530

    added sidebar launcher

--- components/sidebar.js
+++ components/sidebar.js
@@ -14,7 +14,7 @@ angular.
           console.log("data sidebar")
           console.log(data);
         });
-        
+
 
       $scope.data = '[{"file_data":"/media/abc.png","id":2},{"file_data":"/media/pqr.png","id":4}]';
       $scope.message='[{"message_content":"abc","id":4},{"message_content":"hfeu jceeuw hduhewiuwe shuwqhdwu hduwhdwuhdwu duqhi qiuhqi iwmcwknmew ncqwlnlq cqqewhfieo idjqwijdw","id":4}]';
@@ -23,7 +23,7 @@ angular.
 
 
       $scope.fillFiles = function() {
-        
+
         var tags = self.selectedtags.map(function(val) { return val.id});
         var params = {
               userId: root.userId,
@@ -31,7 +31,7 @@ angular.
           };
 
         $http.post(API_URL + "search/file/", params , {async:true}).success(function(data){
-            
+
             $scope.files = data;
             for(var i in $scope.files){
               $scope.files[i].fileName = $scope.files[i].file_data.substring($scope.files[i].file_data.lastIndexOf('/')+1).substring(0, 25);
@@ -50,7 +50,7 @@ angular.
         };
 
         $http.post(API_URL + "search/message/", params,{async:true}).success(function(message){
-            
+
             $scope.messages = message;
           });
 
@@ -59,11 +59,11 @@ angular.
       };
 
       root.$watch('userId', function() { $scope.fillMessages() });
-      $scope.broadcast = function function_name(argument) {
-        $scope.$emit('searchSelectEvent', {
-          data: argument
-        });
-      }
+      // $scope.broadcast = function function_name(argument) {
+      //   $scope.$emit('searchSelectEvent', {
+      //     data: argument
+      //   });
+      // }
 
       $scope.openUrl = function (url) {
         console.log(API_URL + url.substring(1));
