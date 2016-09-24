13a01588283654b8109b14962e74da27717b9ef2
vibhor <vibhor.go@directi.com>
2016-09-24 19:42:39 +0530

    added temp sidebar

--- components/main.js
+++ components/main.js
@@ -6,7 +6,7 @@ angular.
 	module('taggApp').
 	component('home', {
 		templateUrl: 'templates/base.html',
-		controller: [ 
+		controller: [
 			function homeController () {
 				var self = this;
 				//this.name = "harsh";
@@ -14,3 +14,51 @@ angular.
 		]
 
 	});
+//sidebar
+	angular.
+		module('taggApp').
+		component('sidebar', {
+			templateUrl: 'templates/sidebar.html',
+			controller: ['$scope', '$http', function($scope,$http){
+
+			  $scope.data = '{"files":[{"id":"123","file_data":"http://www.example.com/dir/file.html"},{"id":"456","file_data":"http://www.anotherexample.com/directory/hello.img"}]}';
+				$scope.message = '{"messages":[{"id":"123","message_content":"let the game begin"},{"id":"456","message_content":"it was elementary watson"}]}';
+
+
+
+				$scope.fillFiles = function() {
+					$http.get('http').success(function(data){
+				      $scope.data = data;
+				    });
+
+					$scope.files = [];
+					var data = JSON.parse($scope.data);
+					//console.log(data.files);
+					for(var i in data.files){
+						var filename = data.files[i].file_data.substring(data.files[i].file_data.lastIndexOf('/')+1);
+
+						$scope.files.push(filename);
+						//console.log(obj.file_data);
+					}
+					//console.log($scope.skills);
+				}
+
+				$scope.fillMessages = function() {
+					$http.get('http').success(function(data){
+				      $scope.data = data;
+				    });
+
+					$scope.messages = [];
+					var data = JSON.parse($scope.message);
+					//console.log(data.files);
+					for(var i in data.messages){
+						var obj=data.messages[i];
+
+						$scope.messages.push(obj.message_content.substr(0,15));
+						//console.log(obj.file_data);
+					}
+					//console.log($scope.skills);
+				}
+
+			}]
+		});
